15ca9f0c16fe5af03a1712cfeee867d9f3cc6a86
Manisha-500apps <manisha.tatukuluri@500apps.com>
2023-04-11 13:38:11 +0530

    Added Panorama View

---
+++ components/PanoramaView.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- 3D component for rendering the panorama viewer -->
+  <div id="panorama-viewer">
+    <VPannellum
+      v-model:hfov.sync="hfov"
+      v-model:yaw.sync="yaw"
+      v-model:pitch.sync="pitch"
+      :src="urlPath"
+      :preview="urlPath"
+      :auto-rotate="isAutoRotate"
+      :auto-load="true"
+      :show-zoom="false"
+      :show-fullscreen="false"
+      :compass="false"
+    ></VPannellum>
+  </div>
+</template>
+
+<script setup lang="ts">
+import VPannellum from "vue-pannellum";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps(["urlPath"]);
+// Panorama to automatically rotate when loaded
+const isAutoRotate = ref(true);
+// Panorama starting horizontal field of view in degrees
+const hfov = ref(270);
+// Panorama starting yaw position in degrees
+const yaw = ref(0);
+// Panorama starting pitch position in degrees
+const pitch = ref(1);
+</script>
+
+<style>
+#panorama-viewer {
+  width: calc(100vw - 0px) !important;
+  height: calc(100vh - 0px) !important;
+  border-radius: 0px;
+  cursor: grab;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
-  <NuxtPage></NuxtPage>
+  <div></div>
 </template>
+
 <script setup lang="ts"></script>
